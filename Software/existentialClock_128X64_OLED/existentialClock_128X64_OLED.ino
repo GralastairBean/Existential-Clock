43eaf1262be773cc98edd05fefbae0a5ed5505b1
greenalastair <61728985+greenalastair@users.noreply.github.com>
2021-09-30 23:04:22 +0800

    flag system to cycle through different displays

--- Software/existentialClock_128X64_OLED/existentialClock_128X64_OLED.ino
+++ Software/existentialClock_128X64_OLED/existentialClock_128X64_OLED.ino
@@ -19,7 +19,7 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
+// The pins for I2C are defined by the Wire-library.
 // On an arduino UNO:       A4(SDA), A5(SCL)
 // On an arduino MEGA 2560: 20(SDA), 21(SCL)
 // On an arduino LEONARDO:   2(SDA),  3(SCL), ...
@@ -32,9 +32,18 @@ double age = 276936; //31 years = 271560 hours, round to 270000
 double lifeExp = 713064; //81.4 years = 713064 hours
 double hoursLeft = lifeExp - age;
 double pct = (age / lifeExp) * 100;
-unsigned long intervalMillis = 1000; // number of millisecs between updating "age"
-unsigned long currentMillis = 0;    // stores the value of millis() in each iteration of loop()
-unsigned long previousMillis = 0;   // will store last time "age" was updated
+//Time monitors
+
+unsigned long intervalMillis1 = 10000; // number of millisecs between updating "age"
+unsigned long currentMillis1 = 0;    // stores the value of millis() in each iteration of loop()
+unsigned long previousMillis1 = 0;   // will store last time "age" was updated
+
+unsigned long intervalMillis2 = 5000; // number of millisecs between changing data shown on screen
+unsigned long currentMillis2 = 0;    // stores the value of millis() in each iteration of loop()
+unsigned long previousMillis2 = 0;   // will store last time screen data was changed
+
+//flag to change data screen
+int flag = 0;
 
 void setup() {
   // Start debug Serial
@@ -43,29 +52,140 @@ void setup() {
   Wire.begin();
   // initialize OLED with I2C addr 0x3C
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-}
-
-void updateAge() {
   // Clear the display
   display.clearDisplay();
   //Set the color - always use white despite actual display color
   display.setTextColor(WHITE);
   //Set the font size
   display.setTextSize(2);
-  //Set the cursor coordinates
   display.setCursor(0, 0);
+  display.print("Please" );
+  display.setCursor(0, 20);
+  display.print("wait" );
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("Please" );
+  display.setCursor(0, 20);
+  display.print("wait." );
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("Please" );
+  display.setCursor(0, 20);
+  display.print("wait.." );
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("Please" );
+  display.setCursor(0, 20);
+  display.print("wait..." );
+  display.display();
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("EXIST" );
+  display.setCursor(0, 20);
+  display.print("-ENTIAL" );
+  display.setCursor(0, 40);
+  display.print("CLOCK v2");
+  display.display();
+  delay(1500);
+}
+
+
+void updateDisplay() {
+  // Clear the display
+  display.clearDisplay();
+  //Set the color - always use white despite actual display color
+  display.setTextColor(WHITE);
+  //Set the font size
+  display.setTextSize(2);
+  //Check flag settging to decide which data value to display
+  if (flag == 0) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.print("EXIST" );
+    display.setCursor(0, 20);
+    display.print("-ENTIAL" );
+    display.setCursor(0, 40);
+    display.print("CLOCK v2");
+  }
+  else if (flag == 1) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.print("Pct Lived");
+    display.setCursor(10, 32);
+    display.print(pct, 5);
+    display.print("%");
+
+  }
+  else if (flag == 2) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.print("HoursLived");
+    display.setCursor(10, 32);
+    display.print(age, 0);
+
+  }
+  else {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.print("HoursLeft");
+    display.setCursor(10, 32);
+    display.print(hoursLeft, 0);
+
+  }
+
+  //Serial debug output
+  Serial.print("flag:  ");
+  Serial.print(flag);
+  Serial.print("  age:  ");
+  Serial.print(age, 0);
+  Serial.print("  lifeExp:  ");
+  Serial.print(lifeExp, 0);
+  Serial.print("  hoursLeft:  ");
+  Serial.print(hoursLeft, 0);
+  Serial.print("  pct:  ");
+  Serial.print(pct, 6);
+  Serial.println("%");
+}
+
+void updateAge() {
+  // Clear the display
+  //display.clearDisplay();
+  //Set the color - always use white despite actual display color
+  //display.setTextColor(WHITE);
+  //Set the font size
+  //display.setTextSize(2);
+
+  //Set the cursor coordinates
+  //display.setCursor(0, 0);
   //display.print("EXISTENTIAL CLOCK v2");
-  display.setCursor(10, 0);
+  //display.setCursor(10, 0);
   //display.print("time: ");
   //display.setCursor(0, 16);
   //display.print(hoursLeft, 0);
   //display.setCursor(0, 32);
-  display.print("Pct Lived");
-  display.setCursor(10, 32);
-  display.print(pct, 5);
-  display.print("%");
-  
+  //display.print("Pct Lived");
+  //display.setCursor(10, 32);
+  //display.print(pct, 5);
+  //display.print("%");
+
   //Serial debug output
+  Serial.print("flag:  ");
+  Serial.print(flag);
   Serial.print("age:  ");
   Serial.print(age, 0);
   Serial.print("  lifeExp:  ");
@@ -78,13 +198,29 @@ void updateAge() {
 }
 void loop() {
 
-  currentMillis = millis();   //check the time
-  if (currentMillis - previousMillis >= intervalMillis) {
+  currentMillis1 = millis();   //check the time
+  currentMillis2 = millis();
+
+
+  if (currentMillis2 - previousMillis2 >= intervalMillis2) {
+    previousMillis2 += intervalMillis2;
+    flag = flag + 1;
+    if (flag == 4) {
+      flag = 0;
+    }
+    updateAge();
+    updateDisplay();
+    display.display();
+
+  }
+
+  if (currentMillis1 - previousMillis1 >= intervalMillis1) {
     age = age + 1;
     hoursLeft = lifeExp - age;
     pct = (age / lifeExp) * 100;
-    previousMillis += intervalMillis;
+    previousMillis1 += intervalMillis1;
     updateAge();
     display.display();
+
   }
 }
