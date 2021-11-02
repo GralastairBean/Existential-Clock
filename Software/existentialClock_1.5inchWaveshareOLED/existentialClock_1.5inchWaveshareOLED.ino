9f04b7b0ca944d2c2ab8650569cb3bf32fc73364
greenalastair <61728985+greenalastair@users.noreply.github.com>
2021-11-02 15:33:48 +0800

    get 1.5 inch OLED to display basic data

--- Software/existentialClock_1.5inchWaveshareOLED/existentialClock_1.5inchWaveshareOLED.ino
+++ Software/existentialClock_1.5inchWaveshareOLED/existentialClock_1.5inchWaveshareOLED.ino
@@ -36,61 +36,63 @@ void setup()
   GUI_DrawLine(0, 8, sOLED_DIS.OLED_Dis_Column - 1, 8, WHITE, LINE_SOLID , DOT_PIXEL_DFT);
   OLED_DisPage(0, 0);
   OLED_DisPage(0, 7);
-  OLED_ClearBuf();
-
-  GUI_DisString_EN(29, 2, "EXISTENTIAL", &Font12, FONT_BACKGROUND, WHITE);
-  OLED_DisPage(0, 1);
-  OLED_ClearBuf();
-
-  GUI_DisString_EN(37, 2, "CLOCK V3", &Font12, FONT_BACKGROUND, WHITE);
   OLED_DisPage(0, 2);
   OLED_ClearBuf();
 
-  GUI_Disbitmap(0  , 2, Signal816  , 16, 8);
-  GUI_Disbitmap(24 , 2, Bluetooth88, 8 , 8);
-  GUI_Disbitmap(40 , 2, Msg816     , 16, 8);
-  GUI_Disbitmap(64 , 2, GPRS88     , 8 , 8);
-  GUI_Disbitmap(90 , 2, Alarm88    , 8 , 8);
-  GUI_Disbitmap(112, 2, Bat816     , 16, 8);
-  OLED_Display(0, 52, 128, 52 + 16);
+  GUI_DisString_EN(4, 5, "EXISTENTIAL CLOCK", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 1);
   OLED_ClearBuf();
 
-  GUI_DisString_EN(0 , 0, "MUSIC", &Font12, FONT_BACKGROUND, WHITE);
-  GUI_DisString_EN(48, 0, "MENU" , &Font12, FONT_BACKGROUND, WHITE);
-  GUI_DisString_EN(90, 0, "PHONE", &Font12, FONT_BACKGROUND, WHITE);
-  OLED_Display(0, 96, 128, 96 + 16);
-  OLED_ClearBuf();
+
+
+  /*
+    GUI_Disbitmap(0  , 2, Signal816  , 16, 8);
+    GUI_Disbitmap(24 , 2, Bluetooth88, 8 , 8);
+    GUI_Disbitmap(40 , 2, Msg816     , 16, 8);
+    GUI_Disbitmap(64 , 2, GPRS88     , 8 , 8);
+    GUI_Disbitmap(90 , 2, Alarm88    , 8 , 8);
+    GUI_Disbitmap(112, 2, Bat816     , 16, 8);
+    OLED_Display(0, 52, 128, 52 + 16);
+    OLED_ClearBuf();
+
+    GUI_DisString_EN(0 , 0, "MUSIC", &Font12, FONT_BACKGROUND, WHITE);
+    GUI_DisString_EN(48, 0, "MENU" , &Font12, FONT_BACKGROUND, WHITE);
+    GUI_DisString_EN(90, 0, "PHONE", &Font12, FONT_BACKGROUND, WHITE);
+    OLED_Display(0, 96, 128, 96 + 16);
+    OLED_ClearBuf();
+  */
 }
 
 void loop()
 {
-  uint8_t sec = 0;
-  DEV_TIME sDev_time;
-  sDev_time.Hour = 12;
-  sDev_time.Min = 34;
-  sDev_time.Sec = 56;
-  Serial.print("Show time\r\n");
-  for (;;) {
-    sec++;
-    sDev_time.Sec = sec;
-    if (sec == 60) {
-      sDev_time.Min = sDev_time.Min + 1;
-      sec = 0;
-      if (sDev_time.Min == 60) {
-        sDev_time.Hour =  sDev_time.Hour + 1;
-        sDev_time.Min = 0;
-        if (sDev_time.Hour == 24) {
-          sDev_time.Hour = 0;
-          sDev_time.Min = 0;
-          sDev_time.Sec = 0;
-        }
-      }
-    }
-    OLED_ClearBuf();
-    GUI_Showtime(28, 0, 100, 16, &sDev_time, WHITE);
-    OLED_Display(0, 72, 128, 72 + 16);
-    Driver_Delay_ms(1000);//Analog clock 1s
-  }
+  GUI_DisString_EN(15, 5, "YEARS  LEFT:", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 3);
+  OLED_ClearBuf();
+  GUI_DisString_EN(25, 0, "50.40", &Font16, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 5);
+  OLED_ClearBuf();
+  delay(2500);
+  GUI_DisString_EN(18, 5, "DAYS  LEFT:", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 3);
+  OLED_ClearBuf();
+   GUI_DisString_EN(25, 0, "18369", &Font16, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 5);
+  OLED_ClearBuf();
+  delay(2500);
+  GUI_DisString_EN(15, 5, "HOURS  LEFT:", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 3);
+  OLED_ClearBuf();
+   GUI_DisString_EN(25, 0, "441504", &Font16, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 5);
+  OLED_ClearBuf();
+  delay(2500);
+  GUI_DisString_EN(15, 5, "PERCENT  LEFT:", &Font12, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 3);
+  OLED_ClearBuf();
+  GUI_DisString_EN(25, 0, "38.08%", &Font16, FONT_BACKGROUND, WHITE);
+  OLED_DisPage(0, 5);
+  OLED_ClearBuf();
+  delay(2500);
 }
 
 /*********************************************************************************************************
