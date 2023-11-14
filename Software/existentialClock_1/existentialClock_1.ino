9d9f956f82fdcb78be4ba92cd4a59a5159053efc
Alastair Green <61728985+greenalastair@users.noreply.github.com>
2023-11-15 02:22:36 +0800

    formatting only (auto format)

--- Software/existentialClock_1/existentialClock_1.ino
+++ Software/existentialClock_1/existentialClock_1.ino
@@ -1,5 +1,6 @@
 /*existentialClock_1
-Al Green 2021
+  Al Green 2021
+  Uses a 20x4 I2C LCD to show your hours lived, hours to live and % of life
 */
 #include <LiquidCrystal.h>
 #include <Wire.h>
@@ -15,57 +16,67 @@ unsigned long previousMillis = 0;   // will store last time "age" was updated
 
 
 void setup() {
-lcd.setCursor(14, 1);
-lcd.print(" hrs");
+  lcd.init();                      // initialize the lcd
+  lcd.backlight();                  // turn on the lcd backlight
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("EXISTENTIAL CLOCK V1");
 
-lcd.setCursor(0, 2);
-lcd.print("Remain");
-lcd.setCursor(8, 2);
-lcd.print(hoursLeft,0);
-lcd.setCursor(14, 2);
-lcd.print(" hrs");
+  lcd.setCursor(0, 1);
+  lcd.print("Age");
+  lcd.setCursor(8, 1);
+  lcd.print(age, 0);
+  lcd.setCursor(14, 1);
+  lcd.print(" hrs");
 
-lcd.setCursor(0, 3);
-lcd.print("Progress");
-lcd.setCursor(10, 3);
-lcd.print(pct,5);
-lcd.setCursor(18, 3);
-lcd.print("%");
-  
-Serial.begin(9600);
-Serial.print("age:  "); 
-Serial.print(age,0);
-Serial.print("  lifeExp:  "); 
-Serial.print(lifeExp,0);
-Serial.print("  hoursLeft:  "); 
-Serial.print(hoursLeft,0);
-Serial.print("  pct:  "); 
-Serial.print(pct, 7);
-Serial.println("%"); 
+  lcd.setCursor(0, 2);
+  lcd.print("Remain");
+  lcd.setCursor(8, 2);
+  lcd.print(hoursLeft, 0);
+  lcd.setCursor(14, 2);
+  lcd.print(" hrs");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Life %");
+  lcd.setCursor(8, 3);
+  lcd.print(pct, 5);
+  lcd.setCursor(17, 3);
+  lcd.print("%");
+
+  Serial.begin(9600);
+  Serial.print("age:  ");
+  Serial.print(age, 0);
+  Serial.print("  lifeExp:  ");
+  Serial.print(lifeExp, 0);
+  Serial.print("  hoursLeft:  ");
+  Serial.print(hoursLeft, 0);
+  Serial.print("  pct:  ");
+  Serial.print(pct, 7);
+  Serial.println("%");
 
 }
 
 void loop() {
 
-currentMillis = millis();   //check the time
-updateAge();  //call update age function
+  currentMillis = millis();   //check the time
+  updateAge();  //call update age function
 
-if (age >= lifeExp) {
-  Serial.println("***************");
-  Serial.println("YOU ARE DEAD");
-  Serial.println("***************");
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("EXISTENTIAL CLOCK V1");
-  lcd.setCursor(0, 1);
-  lcd.print("********************");
-  lcd.setCursor(0, 2);
-  lcd.print("YOU ARE DEAD.....");
-  lcd.setCursor(0, 3);
-  lcd.print("********************");
-  
-  delay(10000000); 
-  
+  if (age >= lifeExp) {
+    Serial.println("***************");
+    Serial.println("YOU ARE DEAD");
+    Serial.println("***************");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("EXISTENTIAL CLOCK V1");
+    lcd.setCursor(0, 1);
+    lcd.print("********************");
+    lcd.setCursor(0, 2);
+    lcd.print("YOU ARE DEAD.....");
+    lcd.setCursor(0, 3);
+    lcd.print("********************");
+
+    delay(10000000);
+
   }
 }
 
@@ -73,39 +84,41 @@ void updateAge() {
   if (currentMillis - previousMillis >= intervalMillis) {
     age = age + 1;
     hoursLeft = lifeExp - age;
+    pct = (age / lifeExp) * 100;
     previousMillis += intervalMillis;
-    Serial.print("age:  "); 
-    Serial.print(age,0);
-    Serial.print("  lifeExp:  "); 
-    Serial.print(lifeExp,0);
-    Serial.print("  hoursLeft:  "); 
-    Serial.print(hoursLeft,0);
-    Serial.print("  pct:  "); 
+
+    Serial.print("age:  ");
+    Serial.print(age, 0);
+    Serial.print("  lifeExp:  ");
+    Serial.print(lifeExp, 0);
+    Serial.print("  hoursLeft:  ");
+    Serial.print(hoursLeft, 0);
+    Serial.print("  pct:  ");
     Serial.print(pct, 7);
-    Serial.println("%"); 
+    Serial.println("%");
+
     lcd.clear();
     lcd.print("EXISTENTIAL CLOCK V1");
-
     lcd.setCursor(0, 1);
     lcd.print("Age");
     lcd.setCursor(8, 1);
-    lcd.print(age,0);
+    lcd.print(age, 0);
     lcd.setCursor(14, 1);
     lcd.print(" hrs");
 
     lcd.setCursor(0, 2);
     lcd.print("Remain");
     lcd.setCursor(8, 2);
-    lcd.print(hoursLeft,0);
+    lcd.print(hoursLeft, 0);
     lcd.setCursor(14, 2);
     lcd.print(" hrs");
 
     lcd.setCursor(0, 3);
-    lcd.print("Progress");
-    lcd.setCursor(10, 3);
-    lcd.print(pct,5);
-    lcd.setCursor(18, 3);
+    lcd.print("Life %");
+    lcd.setCursor(8, 3);
+    lcd.print(pct, 5);
+    lcd.setCursor(17, 3);
     lcd.print("%");
   }
-  
+
 }
