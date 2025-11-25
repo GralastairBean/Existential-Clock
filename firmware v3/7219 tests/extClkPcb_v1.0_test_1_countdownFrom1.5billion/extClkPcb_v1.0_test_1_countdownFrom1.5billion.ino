38741774d607c292c95f9cc8a691ca5b2cb9c331
Gralastair Bean <greenalastair@gmail.com>
2025-11-25 10:02:34 +0800

    change seconds to real value

--- firmware v3/7219 tests/extClkPcb_v1.0_test_1_countdownFrom1.5billion/extClkPcb_v1.0_test_1_countdownFrom1.5billion.ino
+++ firmware v3/7219 tests/extClkPcb_v1.0_test_1_countdownFrom1.5billion/extClkPcb_v1.0_test_1_countdownFrom1.5billion.ino
@@ -2,7 +2,7 @@
 
 LedControl lc = LedControl(7,6,5,2);
 
-unsigned long n = 1500000000UL;
+unsigned long n = 1514505600UL;
 unsigned long lastTick = 0;
 
 void setup() {
@@ -21,14 +21,13 @@ void displayNumber(unsigned long val) {
   }
 
   bool leading = true;
-  for (int i = 0; i < 12; i++) {
+  for (int i = 11; i >= 0; i--) { // start from most significant
     int d = digits[i];
-    int dev = (11 - i) / 8;   // leftmost digits -> device 0, rightmost -> device 1
-    int pos = (11 - i) % 8;   // left to right inside device
+    int dev = (11 - i) / 8;
+    int pos = (11 - i) % 8;
+    bool dp = (i % 3 == 0 && i != 11); // adjust as needed
 
-    bool dp = (i == 3 || i == 6 || i == 9);  // group every three digits from right
-
-    if (leading && d == 0 && i >= 9) {
+    if (leading && d == 0) {
       lc.setChar(dev,pos,' ',false);
     } else {
       lc.setDigit(dev,pos,d,dp);
