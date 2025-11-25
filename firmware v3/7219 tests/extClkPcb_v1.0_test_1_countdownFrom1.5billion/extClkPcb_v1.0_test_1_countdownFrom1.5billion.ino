22adbff78a467476b0f38b821604871158f7c3bc
Gralastair Bean <greenalastair@gmail.com>
2025-11-25 12:02:29 +0800

    Update extClkPcb_v1.0_test_1_countdownFrom1.5billion.ino

--- firmware v3/7219 tests/extClkPcb_v1.0_test_1_countdownFrom1.5billion/extClkPcb_v1.0_test_1_countdownFrom1.5billion.ino
+++ firmware v3/7219 tests/extClkPcb_v1.0_test_1_countdownFrom1.5billion/extClkPcb_v1.0_test_1_countdownFrom1.5billion.ino
@@ -1,6 +1,9 @@
-#include "LedControl.h"
+// Test of PCB v1.0
+// No RTC, No brightness control
+// Counts down from ~1.5 billion seconds
 
-LedControl lc = LedControl(7,6,5,2);
+#include "LedControl.h"
+LedControl lc = LedControl(7,6,5,2); // Arduino pins: D7=DIN, D6=CLK, D5=LOAD/CS, 2= 2x 7219 driver chips
 
 unsigned long n = 1514505600UL;
 unsigned long lastTick = 0;
